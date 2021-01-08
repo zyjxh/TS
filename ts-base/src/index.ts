9d75319f5a40dc7e572c3f184b1fafb61c706ab9
郑云 <zhengyunk@yonyou.com>
2021-01-08 09:40:18 +0800

    refactor: 新增工程

--- ts-base/src/index.ts
+++ ts-base/src/index.ts
@@ -1,2 +1,9 @@
-let hello: string = 'Hello TypeScript'
-document.querySelectorAll('.app')[0].innerHTML = hello
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: ZhengYun
+ * @Date: 2021-01-03 15:49:27
+ * @LastEditTime: 2021-01-07 17:46:52
+ */
+// let hello: string = 'Hello TypeScript'
+// document.querySelectorAll('.app')[0].innerHTML = hello
